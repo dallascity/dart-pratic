70f493469f9cf6ebcfe0d1d3d378ef0f02d42985
Eymen <dallascity@gmail.com>
2022-10-21 15:12:02 +0300

    İşlem.dart class ve fonksiyonlar oluşturuldu

---
+++ islem.dart
@@ -0,0 +1,19 @@
+// TODO Implement this library.
+
+class islem {
+  void toplama(int a, int b) {
+    print(a + b);
+  }
+
+  void cikarma(int a, int b) {
+    print(a - b);
+  }
+
+  void carpma(int a, int b) {
+    print(a * b);
+  }
+
+  void bolme(int a, int b) {
+    print(a / b);
+  }
+}
---
+++ main.dart
@@ -0,0 +1,8 @@
+import 'islem.dart';
+
+void main(List<String> args) {
+  islem cek = new islem();
+
+ var top = cek.bolme(5, 10);
+
+}
